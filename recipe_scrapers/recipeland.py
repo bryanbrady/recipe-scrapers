9e0d1d009111692df170747dbc714d49dd585da1
Bryan Brady <bryan.brady@gmail.com>
2025-04-24 22:23:36 -0700

    update recipeland ingredient parsing

--- recipe_scrapers/recipeland.py
+++ recipe_scrapers/recipeland.py
@@ -9,30 +9,33 @@ class RecipeLand(AbstractScraper):
         return "recipeland.com"
 
     def _parse_ingredient_row(self, row):
-        amount = row.select_one(".amount") and row.select_one(".amount").get_text()
-        measure = row.select_one(".measure") and row.select_one(".measure").get_text()
-        ingredient_elem = row.select_one(".ingred")
+        amount = row.select_one(".amount-measure .ir-us") and row.select_one(".amount-measure .ir-us").get_text()
+        measure = row.select_one(".ingredient .ir-us") and row.select_one(".ingredient .ir-us").get_text()
+        ingredient_elem = row.select_one("a")
         ingredient = ingredient_elem and ingredient_elem.get_text()
 
         full_ingredient = f"{amount} {measure} {ingredient}"
         return normalize_string(full_ingredient)
 
     def ingredients(self):
-        ingredient_rows = self.soup.select("#ingredient_list tbody tr")
-        return [
+        ingredient_rows = self.soup.select("#ingredient_list .ingredient-row")
+        ingredient_rows = [
             self._parse_ingredient_row(row)
             for row in ingredient_rows
             if not row.select_one("th.i-head")
         ]
+        if ingredient_rows == []:
+            ingredient_rows = self.schema.ingredients()
+        return ingredient_rows
 
     def ingredient_groups(self):
-        ingredient_rows = self.soup.select("#ingredient_list tbody tr")
+        ingredient_rows = self.soup.select("#ingredient_list .ingredient-row")
         groups = []
         current_group = None
         grouped_ingredients = []
 
         for row in ingredient_rows:
-            group_header = row.select_one("th.i-head")
+            group_header = row.select_one(".preparation")
             if group_header:
                 if current_group or grouped_ingredients:
                     groups.append(
